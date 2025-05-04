05c1dc6804c30438b58012461cade006e5299635
oscar <corcasta@outlook.com>
2025-05-04 12:14:52 +0200

    Full API done, but still stuff to do

--- api/routes.py
+++ api/routes.py
@@ -4,7 +4,8 @@ if proj_root not in sys.path:
     sys.path.insert(0, proj_root)
 
 from starlette.datastructures import FormData, UploadFile
-from scripts.utils import batch_preprocessing, batch_extraction
+from fastapi import HTTPException, status
+from scripts.utils import batch_extraction
 from models.tcn import TCNPredictor
 import litserve as ls 
 import pandas as pd
@@ -12,57 +13,114 @@ import numpy as np
 import torch
 import json
 
-DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-RUN = "2025_05_03_22_24_58"
+RUN = "2025_05_04_11_11_35" #"2025_05_03_22_24_58"
 MODEL_WEIGHTS = proj_root+f"/models/weights/tcn_{RUN}.pt"
+DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 
 class ForecastAPI(ls.LitAPI):
     def setup(self, device):
-        self.model = TCNPredictor(input_size=13, seq_len=8, output_size=4).to(DEVICE)
+        self.device = device
+        self.model = TCNPredictor(input_size=13, seq_len=8, output_size=4).to(device)
         self.model.load_state_dict(torch.load(MODEL_WEIGHTS, weights_only=True))
-        #print("setup: ", self.model.device.type)
+        self.model.eval()
+    
 
     def decode_request(self, request, **kwargs):
-        # Product IDs extraction
-        metadata_str = request.get("metadata")
-        metadata = json.loads(metadata_str)
-        product_ids = metadata["product_id"]
-        if len(product_ids) == 0:
-            raise Exception("No products id were given. Provide 1 at least.") 
-        print(product_ids)
+        if "metadata" not in request:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail="Missing metadata key in form-data upload."
+            )
+        
+        if "file" not in request:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail="Missing csv file in form-data upload."
+            )
+        
+        metadata = json.loads(request["metadata"])
+        if "product_id" not in metadata.keys():
+            raise HTTPException(
+                status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+                detail="product_id key is required inside metadata."
+            )
+
+        if len(metadata["product_id"]) == 0:
+            raise HTTPException(
+                status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+                detail="product_id content must have at least 1 element."
+            )
+            
+        self.product_ids = []
+        self.product_ids_invalid = [] 
+        
+        for pid in metadata["product_id"]:
+            pid = int(pid)
+            # For this demo we can do this simple checkup.
+            if pid < 0 or pid > 14:
+                self.product_ids_invalid.append(pid)
+            else:
+                self.product_ids.append(pid)      
+        
+        if len(self.product_ids) == 0:
+            raise HTTPException(
+                status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+                detail="Not a single valid product_id was given. Provide at least 1 (values from 0-14)."
+            )
+  
+        if "file" not in request:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail="Missing CSV file in form-data upload"
+            )
         
         # Dataframe extraction
-        df_upload = request.get("file")
-        df = pd.read_csv(df_upload.file)
-        batch = batch_extraction(df, product_ids)
-        #df = df[df["Product_ID"].isin(product_ids)]
-        # total_amount, Age, Male, Female, Quantity, price_per_unit, year, month, week, window_mean_4, window_mean5, window_mean_6, window_mean7, 
+        try:
+            csv = request["file"].file
+            df = pd.read_csv(csv)
+        except Exception as e:
+            raise HTTPException(
+                status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+                detail=f"Error parsing CSV: {e}"
+            )
+        
+        # Each sample in the batch follows same order as self.product_ids :)
+        batch = batch_extraction(df, self.product_ids)
         print("decode_request_step")
-        print(batch.shape)
-        #print(f"decoded_request: ",type(image))
-        return "image"
+        print(batch)
+        return batch
     
     #def batch(self, inputs):
-    #    print("batch_step")
-    #    #print("batch: ", type(inputs))
-    #    return list(inputs)
+    #    return inputs
     
     def predict(self, x, **kwargs):
-        #clean_results = []
+        predictions = self.model.predict(x.to(DEVICE))
         print("predict_step")
-        #print("predict: ", type(results))
-        return x #[0]
+        print(predictions.shape)
+        return predictions
     
     #def unbatch(self, output):
-    #    print("unbatch_step")
     #    return output
     
     def encode_response(self, output, **kwargs):
         print("encode_response_step")
-        print("========================================")
-        return {"demo": "yes"}
+        if self.device != "cpu":
+            output = output.cpu().numpy()
+        else:
+            output = output.numpy()
+        
+        product_output = {}
+        for i, product_id in enumerate(self.product_ids):
+            weeks_output = {}
+            for week in range(output.shape[1]):
+                weeks_output["week_"+str(week)] = str(output[i,week]) 
+            product_output[str(product_id)] = weeks_output
+
+        print("almost_done")
+        print(product_output)
+        return product_output
 
 if __name__ == "__main__":
     api = ForecastAPI()
-    server = ls.LitServer(api, accelerator="auto", max_batch_size=8, batch_timeout=0.05)
+    server = ls.LitServer(api, accelerator=DEVICE)
     server.run(port=8000)
